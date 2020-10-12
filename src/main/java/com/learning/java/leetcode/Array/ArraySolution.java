3722a925e788c4089bf30b595363c70d51b7b04f
LR <luorui3@lenovo.com>
2020-10-12 22:19:43 +0800

    位移算法

--- src/main/java/com/learning/java/leetcode/Array/ArraySolution.java
+++ src/main/java/com/learning/java/leetcode/Array/ArraySolution.java
@@ -62,4 +62,19 @@ public class ArraySolution {
         return maxProfit;
     }
 
+    /**
+     * 输入: [1,2,3,4,5,6,7] 和 k = 3
+     * 输出: [5,6,7,1,2,3,4]
+     * 解释:
+     * 向右旋转 1 步: [7,1,2,3,4,5,6]
+     * 向右旋转 2 步: [6,7,1,2,3,4,5]
+     * 向右旋转 3 步: [5,6,7,1,2,3,4]
+     *
+     * @param nums 给定一个数组
+     * @param k 将数组中的元素向右移动 k 个位置，其中 k 是非负数
+     */
+    public void rotate(int[] nums, int k) {
+        
+    }
+
 }
